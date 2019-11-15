6dc981ed0775f7765f81f93362faeae7cd5016a3
Ihsaan Ahmad <ihsaan.edu4321@gmail.com>
2019-11-15 10:50:29 -0600

    updating deploy

---
+++ src/components/main/main.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./main.css";
+
+const Wrapper = props => (
+  <div>
+    <main className="content">{props.children}</main>;
+    <div>
+      <footer className="footer">
+        <div className="bottom">
+          Clicky Game
+          <a
+            alt="react"
+            href="https://ihsaan-ahmad.github.io/React-Clicky-Game/"
+          >
+            GitHub
+          </a>
+        </div>
+      </footer>
+    </div>
+  </div>
+);
+
+export default Wrapper;
