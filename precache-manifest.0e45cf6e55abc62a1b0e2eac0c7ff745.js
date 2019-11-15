0a60c5c04b619cbfa843d8a60a754adcab7edbf0
Ihsaan Ahmad <ihsaan.edu4321@gmail.com>
2019-11-15 10:47:51 -0600

    Updates

---
+++ precache-manifest.0e45cf6e55abc62a1b0e2eac0c7ff745.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f212e875e73a9b4dc57e69b8535b0c85",
+    "url": "//ihsaan-ahmad.github.io/React-Clicky-Game/index.html"
+  },
+  {
+    "revision": "bbe6b1ac6b412a24e6f5",
+    "url": "//ihsaan-ahmad.github.io/React-Clicky-Game/static/css/main.73c11658.chunk.css"
+  },
+  {
+    "revision": "38e3e33cdc9a6e01ae5f",
+    "url": "//ihsaan-ahmad.github.io/React-Clicky-Game/static/js/2.052cdbb2.chunk.js"
+  },
+  {
+    "revision": "bbe6b1ac6b412a24e6f5",
+    "url": "//ihsaan-ahmad.github.io/React-Clicky-Game/static/js/main.a01c48e5.chunk.js"
+  },
+  {
+    "revision": "5ac6b10783a658bf27a6",
+    "url": "//ihsaan-ahmad.github.io/React-Clicky-Game/static/js/runtime-main.5eea1154.js"
+  }
+]);
